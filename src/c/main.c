1da32d596466d3ec3dba9e1cadb1b561cf0c14df
Sebastian Thiel <byronimo@gmail.com>
2015-01-08 15:16:08 +0100

    Entirely new version of rps game, as made during re-recorded screencast

--- src/c/main.c
+++ src/c/main.c
@@ -1,37 +1,36 @@
 #include <stdio.h>
-#include <stdbool.h>
 #include <string.h>
 
-const int MAX_ARGUMENT_COUNT = 3;
 const char* choices[] = {"rock", "paper", "scissors"};
-const char* results[] = {"It's a tie", "Player 1 wins", "Player 2 wins"};
+const char* results[] = {"We tied", "Player 2 wins", "Player 1 wins"};
+const int NUM_RESULTS = sizeof(results) / sizeof(results[0]);
+
 
 int input_id(const char* input) {
-	for(const char** choice = choices; choice < choices + sizeof(choices) / sizeof(choices[0]); ++choice) {
-		if (strcmp(input, *choice) == 0) {
-			return choice - choices;
+	for (unsigned long cid = 0; cid < sizeof(choices) / sizeof(choices[0]); ++cid) {
+		if (strcmp(input, choices[cid]) == 0) {
+			return cid;
 		}
 	}
 	return -1;
 }
 
 int main(int argc, char* args[]) {
-	if (argc != MAX_ARGUMENT_COUNT) {
-		fprintf(stderr, "Please provide choices for player 1 and player 2, totalling two arguments\n");
+	if (argc != 3) {
+		fprintf(stderr, "Please provide choices for player one and player two as arguments\n");
 		return 1;
 	}
 
-	int pchoice[] = {0, 0};
-//	for(char** arg = args + 1; arg < args + MAX_ARGUMENT_COUNT; ++arg) {
-	for(int i = 1; i < MAX_ARGUMENT_COUNT; ++i) {
-		const int cindex = i - 1;
-		pchoice[cindex] = input_id(args[i]);
-		if (pchoice[cindex] < 0) {
-			fprintf(stderr, "Invalid game choice '%s'\nvalid player choices are %s, %s, or %s\n", args[i], choices[0], choices[1], choices[2]);
+	int pchoices[] = {0, 0};
+	for (int argi = 1; argi < argc; ++argi) {
+		const int pci = argi - 1;
+		pchoices[pci] = input_id(args[argi]);
+		if (pchoices[pci] < 0) {
+			fprintf(stderr, "Player choice '%s' is invalid, please choose one of %s, %s, or %s\n", args[argi], choices[0], choices[1], choices[2]);
 			return 1;
 		}
 	}
 
-	printf("%s\n", results[((pchoice[0] - pchoice[1]) + 3) % 3]);
+	printf("%s\n", results[((pchoices[1] - pchoices[0]) + NUM_RESULTS) % NUM_RESULTS]);
 	return 0;
 }
